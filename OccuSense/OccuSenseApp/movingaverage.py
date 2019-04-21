67398a14c37da30d3c7400857b44aacc2996530b
Vayum Arora <vayuma@andrew.cmu.edu>
2019-04-21 19:46:44 -0400

    first commit

---
+++ OccuSense/OccuSenseApp/movingaverage.py
@@ -0,0 +1,29 @@
+import requests
+response = requests.get('https://techtutorialsx.com/2016/10/30/python-parsing-json/')
+print(response.text)
+
+
+
+
+
+
+
+
+
+def getAvg(x, size, total): 
+    total = total + x; 
+    return float(sum) / n; 
+  
+
+def streamAvg(array, n): 
+	n = len(array)
+    avg = 0; 
+    total = 0; 
+    for i in range(n): 
+        avg = getAvg(arr[i], i + 1, total); 
+        total = avg * (i + 1); 
+        print("Average of ", end = ""); 
+        print(i + 1, end = ""); 
+        print(" numbers is ", end = ""); 
+        print(avg)
+    return; 
